79441d7dcced6ed4c3d0e04cc38c0fd95f272e26
Rachit7Sharma <amazingrachit27@gmail.com>
2025-04-12 02:18:09 +0530

    📦 Updated project section with icons and styling improvements

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,21 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, icon }) => {
   return (
-    <Col size={12} sm={6} md={4}>
+    <Col sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        {imgUrl ? (
+          <img src={imgUrl} alt={title} />
+        ) : (
+          <div className="icon-placeholder">
+            {icon}
+          </div>
+        )}
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
       </div>
     </Col>
-  )
-}
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,126 +1,26 @@
-// import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-// import { ProjectCard } from "./ProjectCard";
-// import projImg1 from "../assets/img/project-img1.png";
-// import projImg2 from "../assets/img/project-img2.png";
-// import projImg3 from "../assets/img/project-img3.png";
-// import colorSharp2 from "../assets/img/color-sharp2.png";
-// import 'animate.css';
-// import TrackVisibility from 'react-on-screen';
-
-// export const Projects = () => {
-
-//   const projects = [
-//     {
-//       title: "Business Startup",
-//       description: "Design & Development",
-//       imgUrl: projImg1,
-//     },
-//     {
-//       title: "Business Startup",
-//       description: "Design & Development",
-//       imgUrl: projImg2,
-//     },
-//     {
-//       title: "Business Startup",
-//       description: "Design & Development",
-//       imgUrl: projImg3,
-//     },
-//     {
-//       title: "Business Startup",
-//       description: "Design & Development",
-//       imgUrl: projImg1,
-//     },
-//     {
-//       title: "Business Startup",
-//       description: "Design & Development",
-//       imgUrl: projImg2,
-//     },
-//     {
-//       title: "Business Startup",
-//       description: "Design & Development",
-//       imgUrl: projImg3,
-//     },
-//   ];
-
-//   return (
-//     <section className="project" id="projects">
-//       <Container>
-//         <Row>
-//           <Col size={12}>
-//             <TrackVisibility>
-//               {({ isVisible }) =>
-//               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-//                 <h2>Projects</h2>
-//                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-//                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
-//                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-//                     <Nav.Item>
-//                       <Nav.Link eventKey="first">Tab 1</Nav.Link>
-//                     </Nav.Item>
-//                     <Nav.Item>
-//                       <Nav.Link eventKey="second">Tab 2</Nav.Link>
-//                     </Nav.Item>
-//                     <Nav.Item>
-//                       <Nav.Link eventKey="third">Tab 3</Nav.Link>
-//                     </Nav.Item>
-//                   </Nav>
-//                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-//                     <Tab.Pane eventKey="first">
-//                       <Row>
-//                         {
-//                           projects.map((project, index) => {
-//                             return (
-//                               <ProjectCard
-//                                 key={index}
-//                                 {...project}
-//                                 />
-//                             )
-//                           })
-//                         }
-//                       </Row>
-//                     </Tab.Pane>
-//                     <Tab.Pane eventKey="section">
-//                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-//                     </Tab.Pane>
-//                     <Tab.Pane eventKey="third">
-//                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-//                     </Tab.Pane>
-//                   </Tab.Content>
-//                 </Tab.Container>
-//               </div>}
-//             </TrackVisibility>
-//           </Col>
-//         </Row>
-//       </Container>
-//       <img className="background-image-right" src={colorSharp2}></img>
-//     </section>
-//   )
-// }
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { FaRobot, FaHospitalSymbol, FaBitcoin } from "react-icons/fa";
 
 export const Projects = () => {
   const projects = [
     {
       title: "NAMBI 2.0 - Mars Rover",
       description: "Developed autonomous navigation using ROS with LIDAR, stereo/depth cameras, GPS, and IMU. Integrated and optimized the rover’s electronics subsystem for Mars-like terrains.",
-      imgUrl: projImg1,
+      icon: <FaRobot size={80} color="#00d8ff" />,
     },
     {
       title: "Crypto Anveshan",
       description: "Designed frontend using HTML/CSS and built a backend database for managing bond purchases via cryptocurrency. Ensured smooth transactional flow with custom logic.",
-      imgUrl: projImg2,
+      icon: <FaBitcoin size={80} color="#f7931a" />,
     },
     {
       title: "Hospital Management System",
       description: "Built an end-to-end system for doctors and patients using Python, SQL, and PyMySQL. Enabled secure scheduling and medical data storage with unique IDs.",
-      imgUrl: projImg3,
+      icon: <FaHospitalSymbol size={80} color="#e91e63" />,
     },
   ];
 
@@ -131,50 +31,43 @@ export const Projects = () => {
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Projects</h2>
-                <p>Here are a few highlights of my technical journey showcasing software development, embedded systems, and data analytics.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Featured</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Other</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Extra</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                              />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <p>More of my technical work and experiments can be found on my GitHub profile.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Collaborative team challenges and exploratory problem-solving are some things I enjoy the most.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Projects</h2>
+                  <p>My journey in robotics and software development has been driven by curiosity and a love for building things. Below are some projects showcasing embedded systems, automation, and full-stack applications.</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Featured</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Other</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Extra</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => (
+                            <ProjectCard key={index} {...project} />
+                          ))}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>More of my technical experiments and side projects can be explored through my GitHub profile.</p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>I thrive in collaborative environments and love working on interdisciplinary problem-solving challenges.</p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="Background Decoration"/>
     </section>
   )
 }
