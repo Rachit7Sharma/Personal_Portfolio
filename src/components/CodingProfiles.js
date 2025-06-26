b82d72b9522cf6366a2e751428737731bed624cb
Rachit7Sharma <amazingrachit27@gmail.com>
2025-06-26 15:35:11 +0530

    Updated Leetcode Rating

--- src/components/CodingProfiles.js
+++ src/components/CodingProfiles.js
@@ -12,9 +12,9 @@ export const CodingProfiles = () => {
             </div>
             <p>I consistently work on my data structures and algorithms by solving challenges online:</p>
             <ul>
-              <li><a href="https://leetcode.com/u/Rachit7Sharma/" target="_blank">LeetCode - 1521 Rating</a></li>
+              <li><a href="https://leetcode.com/u/Rachit7Sharma/" target="_blank">LeetCode Rating - 1624</a></li>
               <li><a href="https://www.codechef.com/users/rachit7sharma" target="_blank">CodeChef – 2★</a></li>
-              <li><a href="https://codeforces.com/profile/Rachit7Sharma" target="_blank">Codeforces – Newbie (1057)</a></li>
+              <li><a href="https://codeforces.com/profile/Rachit7Sharma" target="_blank">Codeforces – Newbie</a></li>
             </ul>
           </Col>
         </Row>
